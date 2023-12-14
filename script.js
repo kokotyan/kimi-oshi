2c9f47523974b637e3b9964493329333440ba35a
kokotyan <kokochi.0207@gmail.com>
2023-12-14 19:38:31 +0900

    色合いを変えてみました

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 $(document).ready(function () {
-    var $menu = $("#global_nav");
-    var $openBtn = $(".openbtn");
+  var $menu = $("#global_nav");
+  var $openBtn = $(".openbtn");
 
-    $openBtn.click(function (e) {
-        e.stopPropagation();
-        console.log("openBtn clicked");
-        $openBtn.toggleClass('active');
-        $menu.slideToggle(300);
-    });
+  $openBtn.click(function (e) {
+    e.stopPropagation();
+    console.log("openBtn clicked");
+    $openBtn.toggleClass("active");
+    $menu.slideToggle(300);
+  });
 
-    $menu.click(function (e) {
-        e.stopPropagation();
-        console.log("menu clicked");
-    });
+  $menu.click(function (e) {
+    e.stopPropagation();
+    console.log("menu clicked");
+  });
 
-    $(document).click(function (e) {
-        console.log("document clicked");
-        if (!$menu.is(e.target) && $menu.has(e.target).length === 0) {
-            $openBtn.removeClass('active');
-            $menu.slideUp(300);
-        }
-    });
+  $(document).click(function (e) {
+    console.log("document clicked");
+    if (!$menu.is(e.target) && $menu.has(e.target).length === 0) {
+      $openBtn.removeClass("active");
+      $menu.slideUp(300);
+    }
+  });
 });
